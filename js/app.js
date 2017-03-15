9038ec7d7417e84328cd63be9ed98f93fee19457
kylelingat <kylejlingat@gmail.com>
2017-03-15 09:11:34 -1000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,92 @@
+var jsonDataBody = [
+  {
+    "header": "center"
+  },
+  {
+    "div" : {
+      "class": "aside"
+    }
+  }
+];
+
+
+/*var jsonData = [
+  {
+    "ul": [
+    {
+      "class": "entries"
+    }
+          ],
+    
+    "li": [
+    {
+      "h1": "",
+      "p": ""
+    }
+          ]
+  }
+];*/
+
+
+var simplifiedJSON = [
+  {
+    "journalEntry": 0
+    "date": "",
+    "title": ["Why I took this class, What I want out of this class"],
+    "content": ["I took this class because I wanted to get an early learning experience of programming and markup languages.", "Get basic knowledge of HTML and CSS"]
+    "img": undefined
+  }
+  {
+    "journalEntry": "1",
+    "date": "",
+    "title": "Difference between UI and UX",
+    "content": "UX stands for User Experience while UI stands for User Interface",
+    "img": undefined
+  },
+  {
+    "journalEntry": "2",
+    "date": "",
+    "title": "HTML and CSS",
+    "content": "HTML or Hyper Text Markup Language is the way a website is structured while CSS or Cascading Style Sheet deals with the way how the website is styled",
+    "img": undefined
+  }
+  {
+    "journalEntry": "3",
+    "date": "",
+    "title": "Responsive Web Design",
+    "content": "Responsive Web Design is probably something I'm going to have trouble with but is essential in making a well designed website"
+    "img": undefined
+  }
+  {
+    "journalEntry": "4",
+    "date": "",
+    "title": "DigimonGo",
+    "content": "DigimonGo was pretty easy, just had to learn how to use hover and border-radius"
+    "img": undefined
+  }
+  {
+    "journalEntry": "5",
+    "date": "",
+    "title": "Block Family (Media Queries)",
+    "content": "Probably the hardest project we had to do. I wasn't there to learn media queries on Friday so I had to learn how to use them myself. I couldn't find out how to do the extra credit one."
+    "img": undefined
+  }
+  {
+    "journalEntry": "6",
+    "date": "",
+    "title": "Basic Javascript",
+    "content": "Javascript seems to be pretty easy, so far it's been similar to Python in terms of the learning curve. Doing functions is probably the most fun part of doing javascript"
+  }
+  {
+    "journalEntry": "7",
+    "date": "",
+    "title": "Conditionals (Alien)"
+    "content": "At first doing this was pretty hard for me because I tried to do it in a function but then I realized that a function isn't needed, all you had to do was make simple if/else statements."
+  }
+  {
+    "journalEntry": "8",
+    "date": "",
+    "title": "VAC Review"
+    "content": "The probelm our school and most schools are facing are lack of communication for clubs/sports. As a club and sport member I know what it feels like to be the only one not ge the memo because lack of communication. Our app solves this by proving a platform for school clubs and sports. It also includes global school features such as lunch menus, bulletins, and student feedback to school. If I wanted to improve my team's video I would try explaining what our app does more rather than skits. Features that we currently have planned are group and individual messaging, membership status for clubs, digital forms, and reminders. Features I would like to add are ways to set due dates and global chat rooms for each school. Challenges our team faces are mostly working together because this is our first year working together and we have such a big group meaning someone has to take command but nobody does. What I would change is how we work together because like I said, we don't have a leader."
+  }
+]
